42f78e1c51ae3648e20fdf97c6866d8bc083bce8
Sara C <sara.cyganska@gmail.com>
2020-11-08 12:04:58 +0100

    Added project for bank mock

---
+++ PaymentGateway/BankApiMock/Controllers/PaymentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankApiMock.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PaymentController : ControllerBase
+    {
+        [HttpPost]
+        public int Post()
+        {
+            return 1;
+        }
+    }
+}
