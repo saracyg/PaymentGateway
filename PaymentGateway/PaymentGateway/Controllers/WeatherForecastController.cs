0a7ccf83abdcf0e34a05e0eb83c610fa11d22c87
Sara C <sara.cyganska@gmail.com>
2020-11-08 12:37:32 +0100

    Added project for payment gateway

---
+++ PaymentGateway/PaymentGateway/Controllers/WeatherForecastController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PaymentGateway.Contract;
+
+namespace PaymentGateway.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public PaymentResult ProcessPayment(Payment payment)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        public PaymentDetails Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
